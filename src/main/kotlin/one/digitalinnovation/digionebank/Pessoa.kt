511b01d623f52bea4d84cc1ed832e80c88b5a1e7
William Marques <matrix.consultoria@gmail.com>
2021-07-09 16:29:21 -0300

    Initial commit

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,17 @@
+package one.digitalinnovation.digionebank
+
+class Pessoa {
+    var nome: String = "Jether"
+    
+    var cpf: String = "123.123.123-11"
+    private set
+}
+
+fun main() {
+    val jether = Pessoa()
+
+    println(jether)
+    println(jether.nome)
+    println(jether.cpf)
+
+}
