97b7be9db5b807a0cc66d66e52495a27af7e524a
William Marques <matrix.consultoria@gmail.com>
2021-07-09 16:39:20 -0300

    updated to talk about class members

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -2,16 +2,16 @@ package one.digitalinnovation.digionebank
 
 class Pessoa {
     var nome: String = "Jether"
-    
     var cpf: String = "123.123.123-11"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val jether = Pessoa()
 
-    println(jether)
-    println(jether.nome)
-    println(jether.cpf)
-
+    println(jether.pessoaInfo())
 }
