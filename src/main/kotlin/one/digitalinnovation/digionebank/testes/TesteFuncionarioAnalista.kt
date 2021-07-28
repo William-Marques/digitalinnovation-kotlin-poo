aeebb5eb6044a52cb41118bbdaada8edecc83dcc
William Marques <matrix.consultoria@gmail.com>
2021-07-27 21:16:41 -0300

    update to make person and employed abstract

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -1,12 +1,9 @@
 package one.digitalinnovation.digionebank.testes
 
 import one.digitalinnovation.digionebank.Analista
-import one.digitalinnovation.digionebank.Funcionario
-import one.digitalinnovation.digionebank.Pessoa
-import java.math.BigDecimal
+
 
 fun main() {
     val joao = Analista("João Pedro", "1234567899", 2000.0)
-    imprimeRelatorio(joao)
+    ImprimeRelatorioFuncionario.imprime(joao)
 }
-fun imprimeRelatorio(funcionario: Funcionario) = println(funcionario.toString())
